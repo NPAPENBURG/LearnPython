3a88a4b598f742abb8bdbac884b17039a12981e8
Nicholas Papenburg <60982660+NPAPENBURG@users.noreply.github.com>
2024-03-13 10:35:53 -0700

    Create week-1.py

---
+++ course-work/week-1.py
@@ -0,0 +1,59 @@
+#####################################
+#### WEEK 1 OF THE PYTHON COURSE ####
+#####################################
+
+
+# Explain what a variable is. What is its purpose?
+
+# List the different types of Python Datatypes
+
+# What is input function used for?
+
+# Create a variable named color with a string value (Print the result)
+
+# Create a variable named number with an integer value (Print the result)
+
+# Create a variable named game_ended with a boolean value (Print the result)
+
+# Store an input to a variable called age. Convert the input to an integer.
+# Hint: You can use type() to check the datatype of a variable
+# (Print the result)
+
+# Create 2 variables that store inputs from the user.
+# Convert the inputs to integers.
+# Add or Subtract the 2 variables and store the result in a new variable.
+# (Print the result)
+
+# Create 2 string variables and concatenate them.
+# There are 3 ways to concatenate strings. One is using the + operator ,f-string, and the other is using the .format() method.
+# Do all three ways
+# (Print the result)
+
+#######################################################################################################################
+### Extra String Manipulation Questions (Some we might have done in session. Others I want you to research and try) ###
+#######################################################################################################################
+# Using the last variables you created. Print the Len of the strings
+
+# Using the last variables you created. Print the first 3 characters of the strings
+
+# Using the last variables you created. Print the last 3 characters of the strings
+
+# Using the last variables you created. Print the string in uppercase
+
+# Using the last variables you created. Print the string in lowercase
+
+# Using the last variables you created. Print the string in title case
+
+# Using the last variables you created. Print the string in reverse
+
+
+################
+### Projects ###
+################
+# Note: Don't copy and paste code from the internet. Try to solve these problems on your own. If you get stuck, ask for help.
+
+# Have a user input a color and a noun. Print the results of those as a superhero name.
+
+# Create a program that asks the user for their name and age. Then print out a message to the user that tells them the year that they will turn 100 years old.
+
+# Create a Tip Calculator. If the bill was $150.00, split between 5 people, with 12% tip. Each person should pay (150.00 / 5) * 1.12 = 33.6. Format the result to 2 decimal places.
