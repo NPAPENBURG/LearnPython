64cefebc5fd5a77acf4b190f4e2b23e5919ba281
Nicholas Papenburg <60982660+NPAPENBURG@users.noreply.github.com>
2024-03-22 09:32:46 -0700

    Create week-2.py

---
+++ course-work/week-2.py
@@ -0,0 +1,82 @@
+# WEEK 2 COURSE WORK
+
+# Create a Tuple with 6 elements. At least one string, one int, one float, one bool
+# Access the first element of the tuple
+# Access the last element of the tuple
+
+
+# Create a List with 6 elements. At least one string, one int, one float, one bool
+# Access the first element of the list
+# Access the last element of the list
+# Append a new element to the list
+# Insert a new element at index 2
+# Remove an element by value
+# Remove an element by index
+# Reverse the list
+# Get Len of the List
+
+
+# Create a list that has 2 list inside. One clist being 3 elements and the other 2 elements of colors
+# Get the first element of the first list
+# Get the last element of the second list
+# Break each list into its on variable. Don't hard code. (Example list1 = ['green,'yellow,'white'], list2 = ['red', 'blue'])
+# Concatenate the two lists as a variable and then sort it
+
+
+# Create a dictionary with 3 key value pairs. At least one string, one int, one float
+# Access the value of the int by key
+# Change the value of the string by key
+# Get the keys
+# Get the values
+# Get the key value pairs
+# Get the length of the dictionary
+# Add A new key value pair
+# Now Delete a key value pair that is not the one you just added
+
+
+# If statements
+# UNCOMMENTED THE LINE BELOW:
+# colors = ['red', 'blue', 'green', 'yellow', 'white']
+# Right an if statement to see if the color red is in the list. If it is print 'Red is in the list' else print
+# 'Red is not in the list'
+
+# Right an if statement to see if the color purple is in the list. If it is print 'Purple is in the list' else print
+# 'Purple is not in the list' If Pruple is not in list. Add it
+
+# Right an if statement to see if the color red is in the list. If it is print 'Yellow is in the list' else print
+# 'Yellow is not in the list' If Yellow is in the list. Remove it
+
+# UNCOMMENTED THE LINE BELOW:
+# people = {'ted': 1, 'sue': 2, 'joe': 3}
+
+# Right an if statement to see if the name ted is in the dictionary. If it is display what number ted is. If not add ted with the next incremental number
+# # Right an if statement to see if the name luke is in the dictionary. If it is display what number luke is. If not add ted with the next incremental number
+
+# Create virables for each person hard coded by getting the value by key
+# Example ted_value = 1 ( DONT HARD CODE. HINT: HOW DO YOU ACCESS A VALUE BY KEY. YOU DID THIS UP ABOVE)
+# Now that we have all 4 variables. (Should be 4 because we added luke)
+# Do an if statement to check if anyone has a number of 5 or higer. This should be a one line if statement. Do not use any for loops. We aren't there yet
+# If someone have a number higher than 5. Print 'Someone has a number higher than 5' else print 'No one has a number higher than 5'
+
+
+
+## PROJECTS ##
+
+### LOGIN SYSTEM ###
+# UNCOMMENT USER
+# user = {'username': 'nic', 'password': 'password'}
+# Create a login system that asks for a username and password
+# If the username is not found print 'Username not found'
+# If the password is incorrect print 'Password incorrect'
+# If the username and password are correct print 'Login successful'
+
+
+### Contact Management System ###
+# UNCOMMENT CONTACTS:
+# contacts = {'nic': {'phone': '123-456-7890', 'email': 'nic@gmail.com'}, 'joe': {'phone': '123-456-7890', 'email': 'joe@gmail.com'}, 'sue': {'phone': '123-456-7890', 'email': 'sue@gmail.com.'}}
+# Create a contact management system that allows you to add, remove, update, and view contacts
+# When you add a contact you should ask for a name, phone, and email
+# When you remove a contact you should ask for a name
+# When you update a contact you should ask for a name, phone, and email
+# When you view a contact you should ask for a name
+# If the contact is not found print 'Contact not found'
